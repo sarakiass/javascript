e9383957ac36a05db2f03a17986cc6a9a5e6a3d3
Adil Derhali <adil.derhali@gmail.com>
2017-11-20 15:29:45 +0100

    prompt message modified

--- conversion.js
+++ conversion.js
@@ -1,3 +1,3 @@
-var degreCelsius=Number(prompt("entrez la valeur en celsius"));
+var degreCelsius=Number(prompt("Veuillez entrez la valeur en celsius"));
 var fahrenheit=(degreCelsius-32)*5/9;
 alert(degreCelsius+ "°C="+fahrenheit+"°F");
